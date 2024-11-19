b7aad5ce993be41a50fd366d51a3475d8528ac92
tyler-kunce <142066172+tyler-kunce@users.noreply.github.com>
2024-11-18 21:39:05 -0600

    Exercise 2.3 Mentor Feedback

--- A2_Recipe_App/src/recipes/models.py
+++ A2_Recipe_App/src/recipes/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Recipe(models.Model):
+    DIFFICULTY_CHOICES = [
+        ('Easy', 'Easy'),
+        ('Medium', 'Medium'),
+        ('Intermediate', 'Intermediate'),
+        ('Hard', 'Hard')
+    ]
+
     name = models.CharField(max_length=120)
     cooking_time = models.FloatField(help_text='(in minutes)')
     ingredients = models.CharField(max_length=450)
-    difficulty = None
+    difficulty = models.CharField(max_length=50, choices=DIFFICULTY_CHOICES, default='Easy', editable=False)
 
     def __str__(self):
         return str(self.name)
