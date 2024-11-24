7f0b904f90ad2fed26e5ad646cf5e56bcc42fb12
tyler-kunce <142066172+tyler-kunce@users.noreply.github.com>
2024-11-23 19:04:24 -0600

    Exercise 2.5

--- A2_Recipe_App/src/recipes/models.py
+++ A2_Recipe_App/src/recipes/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.shortcuts import reverse
+
 
 # Create your models here.
 class Recipe(models.Model):
@@ -11,8 +13,30 @@ class Recipe(models.Model):
 
     name = models.CharField(max_length=120)
     cooking_time = models.FloatField(help_text='(in minutes)')
-    ingredients = models.CharField(max_length=450)
-    difficulty = models.CharField(max_length=50, choices=DIFFICULTY_CHOICES, default='Easy', editable=False)
+    ingredients = models.TextField()
+    difficulty = models.CharField(max_length=50, choices=DIFFICULTY_CHOICES)
+    pic = models.ImageField(upload_to='recipes', default='no_picture.jpg')
 
     def __str__(self):
         return str(self.name)
+    
+    def get_absolute_url(self):
+        return reverse ('recipes:detail', kwargs={'pk': self.pk})
+    
+    def return_ingredients_as_list(self):
+        return self.ingredients.split(', ')
+        
+    def calculate_difficulty(self):
+        ingredient_len = len(self.return_ingredients_as_list())
+        if self.cooking_time < 10 and ingredient_len < 4:
+            return "Easy"
+        elif self.cooking_time < 10 and ingredient_len >= 4:
+            return "Medium"
+        elif self.cooking_time >= 10 and ingredient_len < 4:
+            return "Intermediate"
+        elif self.cooking_time >= 10 and ingredient_len >= 4:
+            return "Hard"
+    
+    def save(self, *args, **kwargs):
+        self.difficulty = self.calculate_difficulty()
+        super().save(*args, **kwargs)
