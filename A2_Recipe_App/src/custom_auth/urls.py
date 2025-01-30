ad2997072d883b96f3790a12974b6f61224925ca
tyler-kunce <142066172+tyler-kunce@users.noreply.github.com>
2025-01-30 17:11:13 -0600

    Remove A2_Recipe_App from tracking

--- A2_Recipe_App/src/custom_auth/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-from .views import login_view, logout_view
-
-app_name = 'custom_auth'
-
-urlpatterns = [
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
-]
